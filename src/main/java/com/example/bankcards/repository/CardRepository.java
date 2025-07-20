e9120ac15485e60b4ac47c84450b55da9d1e8197
Yulia Vorotintseva <yulia.vorotintseva@gmail.com>
2025-07-20 23:47:00 +0600

    feat: add repository for roles, updated repositories for user and card

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -1,12 +1,19 @@
 package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.Card;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
-    List<Card> findByUser_Id(Long userId);
+    List<Card> findByUserId(Long userId);
+
     boolean existsByCardNumber(String cardNumber);
+
+    <T> Optional<T> findAll(Specification<Card> cardSpecification, Pageable pageable);
 }
---
+++ src/main/java/com/example/bankcards/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(final String name);
+}
